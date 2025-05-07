726acc2a9e2a9a253f7a43fb30af27fd38320f79
Rituraj Paul <ronnieshro@gmail.com>
2025-05-07 15:53:30 +0530

    AI model changed

--- src/Pages/records/single-record-details.jsx
+++ src/Pages/records/single-record-details.jsx
@@ -79,7 +79,7 @@ const SingleRecordDetails = () => {
         },
       ];
 
-      const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
+      const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
       const prompt = `You are an expert cancer and any disease diagnosis analyst. Use your knowledge base to answer questions about giving personalized recommended treatments.
         give a detailed treatment plan for me, make it more readable, clear and easy to understand make it paragraphs to make it more readable`;
 
